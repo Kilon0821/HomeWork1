b6142bdd4af87afe285f1301f6c5e1568caec15a
Kiang <Kiang@Kilon>
2016-10-06 16:32:41 +0800

    change String

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -4,6 +4,26 @@ public class hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello FCU!");
+		int i;
+		String[] lyrics="Let it go ! Let it go ! Cannot hold it back anymore".split("\\s");
+		
+		for(i=0;i<lyrics.length;i++)
+		{
+			//System.out.println(lyrics[i]);
+			if(lyrics[i].equals("it")==true)
+			{
+				lyrics[i]=lyrics[i].replace(lyrics[i],"her");
+			}
+			if(lyrics[i].equals("Cannot")==true)
+			{
+				lyrics[i]=lyrics[i].replace(lyrics[i],"Can't");
+			}
+			else
+			{
+			}
+		}
+		
+		
+		for(i=0;i<lyrics.length;i++)System.out.print(lyrics[i]+" ");
 	}
 }
