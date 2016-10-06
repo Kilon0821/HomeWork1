6bf57219ede5d4455828579cf3c2a74de552247e
Kiang <Kiang@Kilon>
2016-10-06 16:33:59 +0800

    Change String

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -1,29 +1,22 @@
 package hello;
 
-public class hello 
-{
-	public static void main(String[] args)
-	{
+public class hello {
+	public static void main(String[] args) {
 		int i;
-		String[] lyrics="Let it go ! Let it go ! Cannot hold it back anymore".split("\\s");
-		
-		for(i=0;i<lyrics.length;i++)
-		{
-			//System.out.println(lyrics[i]);
-			if(lyrics[i].equals("it")==true)
-			{
-				lyrics[i]=lyrics[i].replace(lyrics[i],"her");
+		String[] lyrics = "Let it go ! Let it go ! Cannot hold it back anymore".split("\\s");
+
+		for (i = 0; i < lyrics.length; i++) {
+			// System.out.println(lyrics[i]);
+			if (lyrics[i].equals("it") == true) {
+				lyrics[i] = lyrics[i].replace(lyrics[i], "her");
 			}
-			if(lyrics[i].equals("Cannot")==true)
-			{
-				lyrics[i]=lyrics[i].replace(lyrics[i],"Can't");
-			}
-			else
-			{
+			if (lyrics[i].equals("Cannot") == true) {
+				lyrics[i] = lyrics[i].replace(lyrics[i], "Can't");
+			} else {
 			}
 		}
-		
-		
-		for(i=0;i<lyrics.length;i++)System.out.print(lyrics[i]+" ");
+
+		for (i = 0; i < lyrics.length; i++)
+			System.out.print(lyrics[i] + " ");
 	}
 }
